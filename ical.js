6f384b7db7ff9d3e91d667575aa3678a5c56a281
Bastian-Noel <71983581+Bastian-Noel@users.noreply.github.com>
2025-09-04 07:45:19 +0200

    UTC+2

--- ical.js
+++ ical.js
@@ -1,12 +1,10 @@
 const { default: ical } = require("ical-generator");
 const he = require("he");
+const { DateTime } = require("luxon"); // pratique pour gérer les TZ
 
-// Map l'événement Celcat brut vers l'événement prêt pour l'ICS
 function mapEvent(event) {
-  // Calcul du module
   const moduleName = event.module ? (event.module.match(/- (.*) \[/) || [])[1] || "" : "";
 
-  // Calcul du tag catégorie
   let categoryTag = "";
   switch (event.eventCategory) {
     case "Cours Magistraux (CM)":
@@ -22,26 +20,24 @@ function mapEvent(event) {
       categoryTag = event.eventCategory ? `[${event.eventCategory}]` : "";
   }
 
-  // Summary
   const summary = moduleName ? `${moduleName} ${categoryTag}` : categoryTag;
 
-  // Description : salle + enseignant
-  const description = `${event.teacher}`;
-
   return {
-    start: new Date(event.start),
-    end: new Date(event.end),
+    start: DateTime.fromISO(event.start, { zone: "Europe/Paris" }).toJSDate(),
+    end: DateTime.fromISO(event.end, { zone: "Europe/Paris" }).toJSDate(),
     allDay: event.allDay,
     summary: he.decode(summary),
-    description: he.decode(description),
+    description: he.decode(`${event.teacher}`),
     location: event.room,
   };
 }
 
 function generateICal(events, group) {
-  const cal = ical({ name: group });
+  const cal = ical({
+    name: group,
+    timezone: "Europe/Paris", // important !
+  });
 
-  // Mapper chaque événement avant de créer l'ICS
   events.map(mapEvent).forEach((event) => {
     cal.createEvent(event);
   });
